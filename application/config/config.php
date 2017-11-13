88c77932b717c44e29683986c8bff01080b3c6e4
taukeer <taukeer.liaqat@gmail.com>
2017-11-14 00:43:00 +0500

    model, controller name made ucfirst

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/admition_system/';
+$config['base_url'] = 'http://local.admission_system.com/';
 
 /*
 |--------------------------------------------------------------------------
